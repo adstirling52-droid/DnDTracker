d530472e3e50395b3e4c2b56bed0bae2ea513466
Alan Stirling <Alan.Stirling@witherbys.com>
2026-04-29 10:47:02 +0100

    Initial WPF app shell with CampaignListWindow

---
+++ DnDTracker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DnDTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
